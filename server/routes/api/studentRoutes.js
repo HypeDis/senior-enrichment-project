ec2e21fb6d9e230fc0d8b4c607fe7e6ce2f7a327
HypeDis <m.bae@live.com>
2019-07-02 12:59:02 -0400

    added single student route

--- server/routes/api/studentRoutes.js
+++ server/routes/api/studentRoutes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const { Student } = require('./../../db');
+const { Student, Campus } = require('./../../db');
 
+// /api/students
 router.get('/', (req, res, next) => {
   Student.findAll()
     .then(students => res.send(students))
+    .catch(next);
+});
+
+// /api/students/:studentId
+router.get('/:studentId', (req, res, next) => {
+  const studentId = req.params.studentId;
+  Student.findByPk(studentId, { include: [Campus] })
+    .then(student => {
+      res.send(student);
+    })
     .catch(next);
 });
 
